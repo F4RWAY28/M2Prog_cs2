468a8cc512b689007a96113a32c5fc0aa556b745
D'onell/F4RWAY <donfrank2007@gmail.com>
2024-12-03 12:23:26 +0100

    update

---
+++ Constructors/Constructors/Quiz.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Quiz
+    {
+        internal QuizVraag[] vragen;
+        internal QuizVraagAntwoord[] ingevuldeAntwoorden;
+    internal Quiz(int aantalVragen)
+    {
+            vragen = new QuizVraag[aantalVragen];
+            ingevuldeAntwoorden = new QuizVraagAntwoord[aantalVragen];
+    }
+        internal void VoegVraagToeOpIndex(int index, QuizVraag vraag)
+        {
+            vragen[index] = vraag;
+
+        }
+
+        internal void VoegVraagToeOpIndex(int index, string vraag, string antwoord)
+        {
+            vragen[index] = new QuizVraag(vraag, antwoord);
+
+            internal void (int vraagIndex)
+            {
+                QuizVraag vraag = vragen[index];
+            }
+        }
+
+    }
+
+    
+}
---
+++ Constructors/Constructors/QuizVraagAntwoord.cs
@@ -0,0 +1,102 @@
+﻿using Constructors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class QuizVraagAntwoord
+    {
+        internal QuizVraag vraag;
+
+        internal bool goed;
+        internal QuizVraagAntwoord(QuizVraag vraag)
+
+        {
+            this.vraag = vraag;
+            this.goed = false;
+        }
+        QuizVraagAntwoord quizVraagAntwoord = new QuizVraagAntwoord(vraag);
+
+
+Console.Writeline
+
+        if(QuizVraagAntwoord = false)
+        {
+        Console.Writeline: ("Goed gedaan!")
+
+            
+        }
+    }
+    
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//??? Ik begrijp het niet zo goed, de betekenis van alles, ik weet niet precies hoe, wat, waar en waarom.
