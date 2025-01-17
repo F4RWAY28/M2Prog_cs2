46b216d2921367155802039478cfaf70c430876e
D'onell/F4RWAY <donfrank2007@gmail.com>
2025-01-17 10:43:50 +0100

    update

---
+++ Dictionaries/Dictionaries/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Dictionaries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,string> personeelOpNummer = new Dictionary<int,string>();
+
+            personeelOpNummer.Add(382942, "jim");
+            personeelOpNummer.Add(789432, "tim");
+
+            string jim = personeelOpNummer[382942];
+            string tim = personeelOpNummer[789432];
+
+            foreach (KeyValuePair<int, string> item in personeelOpNummer)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+
+            foreach (int key in personeelOpNummer.Keys)
+            {
+                Console.WriteLine(key); 
+            }
+
+            foreach (string value  in personeelOpNummer.Values)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
---
+++ Removes/Removes/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Removes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> reviews = new List<double>()
+            { 9.99, 342.97,12.2 };
+            
+            Console.WriteLine(...);
+        }
+
+        
+    }
+}
